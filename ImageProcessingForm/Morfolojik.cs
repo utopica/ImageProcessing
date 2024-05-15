11618e006c19445486fba785505fede609f94bf6
alpayyy <115874416+alpayyy@users.noreply.github.com>
2024-05-15 13:43:43 +0300

    Morfolojik işlemler

--- ImageProcessingForm/Morfolojik.cs
+++ ImageProcessingForm/Morfolojik.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace ImageProcessingForm
@@ -14,18 +9,16 @@ namespace ImageProcessingForm
     public partial class Morfolojik : Form
     {
         private Bitmap originalImage;
-        public Morfolojik(Bitmap mainmainFormImage)
+
+        public Morfolojik(Bitmap mainFormImage)
         {
             InitializeComponent();
-
             this.StartPosition = FormStartPosition.CenterScreen;
 
             string parentDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
-
             string imagePath = Path.Combine(parentDirectory, "Images", "girl.jpg");
 
             originalImage = new Bitmap(imagePath);
-
             DisplayImage(originalImage);
 
             comboBox1.Items.Add("Genişleme - Dilation");
@@ -35,51 +28,118 @@ namespace ImageProcessingForm
 
             // ComboBox'ın SelectedIndexChanged olayını dinleyelim
             comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
-
         }
+
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
             string selectedOperation = comboBox1.SelectedItem.ToString();
+            Bitmap processedImage = null;
+
             switch (selectedOperation)
             {
                 case "Genişleme - Dilation":
-                    Dilation();
+                    processedImage = Dilation(originalImage);
                     break;
                 case "Aşınma - Erosion":
-                    Erosion();
+                    processedImage = Erosion(originalImage);
                     break;
                 case "Açma - Opening":
-                    Opening();
+                    processedImage = Opening(originalImage);
                     break;
                 case "Kapama - Closing":
-                    Closing();
+                    processedImage = Closing(originalImage);
                     break;
-                
                 default:
                     MessageBox.Show("Geçersiz işlem seçimi.");
                     break;
             }
+
+            if (processedImage != null)
+                DisplayImage(processedImage);
         }
 
-        private void Closing()
+        private Bitmap Dilation(Bitmap image)
         {
-            throw new NotImplementedException();
+            Bitmap result = new Bitmap(image.Width, image.Height);
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color maxColor = Color.Black;
+
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            int newX = x + i;
+                            int newY = y + j;
+
+                            if (newX >= 0 && newX < image.Width && newY >= 0 && newY < image.Height)
+                            {
+                                Color currentColor = image.GetPixel(newX, newY);
+
+                                if (currentColor.R > maxColor.R)
+                                    maxColor = currentColor;
+                            }
+                        }
+                    }
+
+                    result.SetPixel(x, y, maxColor);
+                }
+            }
+
+            return result;
         }
 
-        private void Opening()
+        private Bitmap Erosion(Bitmap image)
         {
-            throw new NotImplementedException();
+            Bitmap result = new Bitmap(image.Width, image.Height);
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color minColor = Color.White;
+
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            int newX = x + i;
+                            int newY = y + j;
+
+                            if (newX >= 0 && newX < image.Width && newY >= 0 && newY < image.Height)
+                            {
+                                Color currentColor = image.GetPixel(newX, newY);
+
+                                if (currentColor.R < minColor.R)
+                                    minColor = currentColor;
+                            }
+                        }
+                    }
+
+                    result.SetPixel(x, y, minColor);
+                }
+            }
+
+            return result;
         }
 
-        private void Erosion()
+        private Bitmap Opening(Bitmap image)
         {
-            throw new NotImplementedException();
+            // Opening işlemi, önce Erosion sonra Dilation işlemi uygulamaktadır.
+            Bitmap erodedImage = Erosion(image);
+            Bitmap openedImage = Dilation(erodedImage);
+            return openedImage;
         }
 
-        private void Dilation()
+        private Bitmap Closing(Bitmap image)
         {
-            throw new NotImplementedException();
+            // Closing işlemi, önce Dilation sonra Erosion işlemi uygulamaktadır.
+            Bitmap dilatedImage = Dilation(image);
+            Bitmap closedImage = Erosion(dilatedImage);
+            return closedImage;
         }
 
         private void beforePic_Click(object sender, EventArgs e)
@@ -94,7 +154,6 @@ namespace ImageProcessingForm
             afterPic.Image = null;
         }
 
-
         private void btnDel_Click(object sender, EventArgs e)
         {
             beforePic.Image = null;
@@ -105,32 +164,29 @@ namespace ImageProcessingForm
         {
             SaveImage();
         }
+
         private void DisplayImage(Bitmap image)
         {
             beforePic.Image = image;
-
             afterPic.SizeMode = PictureBoxSizeMode.Zoom;
             beforePic.SizeMode = PictureBoxSizeMode.Zoom;
-
-
         }
+
         private void DownloadImage()
         {
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
 
             openFileDialog1.Filter = "Resim Dosyaları|*.jpg;*.jpeg;*.png;*.gif;*.bmp|Tüm Dosyalar|*.*";
+
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 beforePic.Image = null;
                 afterPic.Image = null;
 
                 beforePic.Image = new Bitmap(openFileDialog1.FileName);
-
                 DisplayImage((Bitmap)beforePic.Image);
             }
         }
-
-
 
         private void SaveImage()
         {
@@ -156,7 +212,5 @@ namespace ImageProcessingForm
                 MessageBox.Show("Kaydedilecek resim bulunmamaktadır.");
             }
         }
-
-        
     }
 }

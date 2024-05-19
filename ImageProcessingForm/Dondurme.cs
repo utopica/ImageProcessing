3785d3fe3e98bb10a1e3593f1a6505b92bc94acf
alpayyy <115874416+alpayyy@users.noreply.github.com>
2024-05-19 16:34:26 +0300

    update

--- ImageProcessingForm/Dondurme.cs
+++ ImageProcessingForm/Dondurme.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,48 +14,35 @@ namespace ImageProcessingForm
     public partial class Dondurme : Form
     {
         private Bitmap defaultImage;
-        
-      
+
         public Dondurme()
         {
             InitializeComponent();
 
-            
             this.StartPosition = FormStartPosition.CenterScreen;
+
 
             string parentDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
 
+
             string imagePath = Path.Combine(parentDirectory, "Images", "girl.jpg");
+
 
             defaultImage = new Bitmap(imagePath);
             beforePic.Image = defaultImage;
             afterPic.SizeMode = PictureBoxSizeMode.Zoom;
             beforePic.SizeMode = PictureBoxSizeMode.Zoom;
-
         }
 
-        private void beforePic_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void afterPic_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
             if (beforePic.Image != null)
             {
-                // Döndürme açısını al
                 int angle;
                 if (int.TryParse(txtAngle.Text, out angle))
                 {
-                    // Resmi döndür
-                    Bitmap rotatedImage = RotateImage((Bitmap)beforePic.Image, angle);
-
-                    // PictureBox'ta döndürülmüş resmi göster
+                    Bitmap rotatedImage = RotateImageWithNearestNeighbor((Bitmap)beforePic.Image, angle);
                     afterPic.Image = rotatedImage;
                 }
                 else
@@ -69,38 +57,35 @@ namespace ImageProcessingForm
         }
 
 
-        private Bitmap RotateImage(Bitmap image, int angle)
+        private Bitmap RotateImageWithNearestNeighbor(Bitmap image, int angle)
         {
-            // Radyan cinsinden açıyı hesapla
             double radianAngle = angle * Math.PI / 180.0;
-
-            // Resmin boyutlarını al
             int width = image.Width;
             int height = image.Height;
-
-            // Yeni bir boş resim oluştur
             Bitmap rotatedImage = new Bitmap(width, height);
 
-            // Döndürülmüş resmi oluştur
+            int centerX = width / 2;
+            int centerY = height / 2;
+
             for (int x = 0; x < width; x++)
-            {
+            {//orijinal görüntünün merkez koordinatlarını hesaplıyoruz.İç içe döngülerde, her bir pikselin yeni konumunu hesaplamak için nearest uyguluyoruz.
+
+
                 for (int y = 0; y < height; y++)
                 {
-                    // Yeni koordinatları hesapla
-                    int newX = (int)(Math.Cos(radianAngle) * (x - width / 2) - Math.Sin(radianAngle) * (y - height / 2) + width / 2);
-                    int newY = (int)(Math.Sin(radianAngle) * (x - width / 2) + Math.Cos(radianAngle) * (y - height / 2) + height / 2);
+                    int newX = (int)(((double)(x - centerX)) * Math.Cos(radianAngle) - ((double)(y - centerY)) * Math.Sin(radianAngle)) + centerX;
+                    int newY = (int)(((double)(x - centerX)) * Math.Sin(radianAngle) + ((double)(y - centerY)) * Math.Cos(radianAngle)) + centerY;
 
-                    // Kenar kontrolü yap
                     if (newX >= 0 && newX < width && newY >= 0 && newY < height)
                     {
-                        rotatedImage.SetPixel(x, y, image.GetPixel(newX, newY));
+                        Color nearestColor = image.GetPixel(newX, newY);
+                        rotatedImage.SetPixel(x, y, nearestColor);
                     }
                 }
             }
 
             return rotatedImage;
         }
-
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
@@ -109,17 +94,10 @@ namespace ImageProcessingForm
 
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                // Kullanıcı tarafından seçilen resmi yükle
                 string imagePath = openFileDialog.FileName;
                 Bitmap selectedImage = new Bitmap(imagePath);
-
-                // Seçilen resmi beforePic'te göster
                 beforePic.Image = selectedImage;
-
-                // Yeni resmi orijinal resim olarak ayarla
                 defaultImage = new Bitmap(selectedImage);
-
-                // İşlenmiş görüntüyü temizleyin
                 afterPic.Image = null;
             }
 
@@ -127,7 +105,6 @@ namespace ImageProcessingForm
 
         private void BtnSave_Click(object sender, EventArgs e)
         {
-            // "Save" butonuna basıldığında burada resmi kaydedebilirsiniz
             SaveFileDialog saveFileDialog1 = new SaveFileDialog();
             saveFileDialog1.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *.png)|*.jpg; *.jpeg; *.gif; *.bmp; *.png";
             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
@@ -141,12 +118,9 @@ namespace ImageProcessingForm
         {
             afterPic.Image = null;
             beforePic.Image = null;
-
         }
 
-        private void txtAngle_TextChanged(object sender, EventArgs e)
-        {
 
-        }
     }
 }
+

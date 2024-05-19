1e456a416e9db76946e727a032cfcd91e998ddf6
alpayyy <115874416+alpayyy@users.noreply.github.com>
2024-05-19 16:46:22 +0300

    düzenleme

--- ImageProcessingForm/Dondurme.cs
+++ ImageProcessingForm/Dondurme.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+
 using System.IO;
+
+using System.IO; 
+
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,13 +32,18 @@ namespace ImageProcessingForm
             string imagePath = Path.Combine(parentDirectory, "Images", "girl.jpg");
 
 
+
+   
+
+         
+
             defaultImage = new Bitmap(imagePath);
             beforePic.Image = defaultImage;
             afterPic.SizeMode = PictureBoxSizeMode.Zoom;
             beforePic.SizeMode = PictureBoxSizeMode.Zoom;
         }
 
-
+    
         private void btnStart_Click(object sender, EventArgs e)
         {
             if (beforePic.Image != null)
